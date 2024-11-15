c557933314802e7723f4c8b1883ef1b44245953d
siren <sing.siren.8@gmail.com>
2024-11-15 22:41:51 +0900

    コメントを追加

--- src/sample7_11.kt
+++ src/sample7_11.kt
@@ -1,5 +1,8 @@
     fun main() {
         val points = Array(4) { IntArray(10) }
+        //多次元配列を生成する構文 Array(size{IntArray(size)}
+        //第一引数はサイズ、第二引数は要素を指定
+        //Arrayは型を明示できる。Array<IntArray>
 
         points[0] = intArrayOf(27, 53, 82, 77, 37, 61, 84, 72, 25, 91)
         points[1] = intArrayOf(54, 76, 96, 39, 18, 45, 89, 92, 68, 88)
