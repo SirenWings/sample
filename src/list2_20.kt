cc8a75f2cf5c98e7598f195e0721f982d822a52b
siren <sing.siren.8@gmail.com>
2024-11-23 23:12:47 +0900

    アナライズしてコメントを追加

--- src/list2_20.kt
+++ src/list2_20.kt
@@ -6,10 +6,16 @@ fun main() {
     //変数listの要素０〜２を足してtotalAに代入
 
     val totalB = tax(list[0]) + tax(list[1]) + tax(list[2])
-    //１３行目のtax関数に渡す値
+    //１３行目のtax関数に渡す値  1000, 2500, 4780が、
+    // return後　1100, 2750, 5285に置き換わる
+
     println("total:${totalA},whith tax:${totalB}")
 }
 
-fun tax(price:Int):Int { //tax関数(引数price:Int型):戻り値Int
-    return (price * 1.1).toInt() //
+fun tax(price:Int):Int {
+    //tax関数。８行目の値（1000, 2500, 4780 ）を受取り、priceに代入。
+    return (price * 1.1).toInt() //1.1を掛けているので.Int()でキャスト
+    //priceそれぞれに1.1を掛けて税込みにして、return。
+    //1100はtax(list[0])に、2750はtax(list[1])に、
+    // 5285はtax(list[2])に置き換わる。
 }
