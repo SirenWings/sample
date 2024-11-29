fcd8083f72f12b6e34b9fdad216bc35a9b301aa0
siren <sing.siren.8@gmail.com>
2024-11-29 22:27:30 +0900

    コメント追加

--- src/list2_26.kt
+++ src/list2_26.kt
@@ -1,11 +1,11 @@
 //ラムダ式で処理を実行するコード
 fun main() {
     val price = 12500
-    val pA = syori(price,10)
-    val pB = syori(price,20)
+    syori(price,10)
+    syori(price,20)
 }
 
 val syori = {p:Int, r:Int ->
     val re = (p * (100.0 + r) / 100.0).toInt()
-    println("price:${p},rate:${r},with tax:${re}")
+    println("price:${p},rate:${r},with tax:$re")
 }
