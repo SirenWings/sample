281ac8ff9eb7afacb2a4f3c5659262a47d2ed244
siren <sing.siren.8@gmail.com>
2024-12-01 23:04:16 +0900

    クラスの定義

---
+++ src/list3_1.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val me = Person() //インスタンスmeを作成
+    me.name = "Taro" //プロパティに値を設定
+    me.mail = "Taro@yamada" //　　〃
+    println(me.say()) //メソッドの呼び出し
+}
+
+class Person {//Personというクラスを定義
+    var name = "noname"
+    var mail = ""
+
+    fun say() = "Name:${name},Email:${mail}"
+}
